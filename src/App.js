ffe41785fab50f85b0946a563f892c60a42a3ca1
shameemahsan1 <shameem8590gmail.com>
2023-08-13 00:41:07 +0600

    shameemahsan msp soft

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Side from './pages/home/Side';
+function App() {
+  return (
+    <div>
+      <Side/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../pages/home/Navbar";
+
+function header() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Sidebar /> */}
+    </div>
+  );
+}
+
+export default header;
